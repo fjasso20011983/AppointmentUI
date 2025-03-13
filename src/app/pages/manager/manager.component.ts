e44de86e188eff998b7fa54b1f94b2b0a733cd2a
fjasso20011983 <fabiola.jasso@unosquare.com>
2025-03-13 12:42:50 -0600

    update appointment form

--- src/app/pages/manager/manager.component.ts
+++ src/app/pages/manager/manager.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { ListViewComponent } from '../../components/list-view/list-view.component';
-import { AppointmentFormComponent } from '../../components/appointment-form/appointment-form.component';
 
 @Component({
   selector: 'app-manager',
-  imports: [ListViewComponent, AppointmentFormComponent],
+  imports: [ListViewComponent],
   templateUrl: './manager.component.html',
   styleUrl: './manager.component.css'
 })
